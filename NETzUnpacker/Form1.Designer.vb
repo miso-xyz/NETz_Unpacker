789267ef24d1cefc2712d3182f87f0bcbb9aeca7
miso-xyz <61099345+miso-xyz@users.noreply.github.com>
2020-09-14 21:25:21 +0100

    Add files via upload

---
+++ NETzUnpacker/Form1.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(156, 240)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label1.Location = New System.Drawing.Point(156, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(188, 240)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "unpacker by misonothx" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "tested on NETz 0.4.8" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "sinister.ly <3" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "--------------" & _
+            "-" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "drop file"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 500
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(344, 240)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "NETz Unpacker"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+
+End Class
---
+++ NETzUnpacker/Form1.vb
@@ -0,0 +1,166 @@
+﻿Imports System.IO
+Imports System.Drawing.Text
+Imports System.Resources
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports ICSharpCode.SharpZipLib.Zip.Compression.Streams
+Public Class Form1
+    Private Shared rm As ResourceManager
+    Private Shared inResourceResolveFlag As Boolean = False
+    Private Shared xrRm As ArrayList
+    Private Shared targettedAssembly As String
+    Private Shared myFonts As PrivateFontCollection
+    Private Shared fontBuffer As IntPtr
+
+    Public Sub New()
+        InitializeComponent()
+
+        If myFonts Is Nothing Then
+            myFonts = New PrivateFontCollection()
+            Dim font As Byte() = My.Resources.font
+            fontBuffer = Marshal.AllocCoTaskMem(font.Length)
+            Marshal.Copy(font, 0, fontBuffer, font.Length)
+            myFonts.AddMemoryFont(fontBuffer, font.Length)
+        End If
+    End Sub
+
+    Protected Overrides Sub OnPaint(ByVal e As PaintEventArgs)
+        Dim fam As FontFamily = myFonts.Families(0)
+        Label1.Font = New Font(fam, 6.0F)
+    End Sub
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.AllowDrop = True
+    End Sub
+
+    Private Sub Form1_DragDrop(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles Me.DragDrop
+        Dim files() As String = e.Data.GetData(DataFormats.FileDrop)
+        For Each path In files
+            targettedAssembly = path
+            Label1.Text = "unpacker by misonothx" & vbCrLf & vbCrLf & "tested on NETz 0.4.8" & vbCrLf & vbCrLf & "sinister.ly <3" & vbCrLf & vbCrLf & "---------------" & vbCrLf & vbCrLf & "loading..."
+            StartApp()
+        Next
+    End Sub
+    Public Function StartApp() As Integer
+        Dim resource As Byte()
+        Try
+            resource = GetResource("A6C24BF5-3690-4982-887E-11E1B159B249")
+        Catch ex As Exception
+            Label1.Text = "unpacker by misonothx" & vbCrLf & vbCrLf & "tested on NETz 0.4.8" & vbCrLf & vbCrLf & "sinister.ly <3" & vbCrLf & vbCrLf & "---------------" & vbCrLf & vbCrLf & "resource not found"
+            Return 0
+        End Try
+        Try
+            GetAssembly(resource)
+            Label1.Text = "unpacker by misonothx" & vbCrLf & vbCrLf & "tested on NETz 0.4.8" & vbCrLf & vbCrLf & "sinister.ly <3" & vbCrLf & vbCrLf & "---------------" & vbCrLf & vbCrLf & "extracting..."
+        Catch ex As Exception
+            Label1.Text = "unpacker by misonothx" & vbCrLf & vbCrLf & "tested on NETz 0.4.8" & vbCrLf & vbCrLf & "sinister.ly <3" & vbCrLf & vbCrLf & "---------------" & vbCrLf & vbCrLf & "failed to extract"
+            Return 0
+        End Try
+        Label1.Text = "unpacker by misonothx" & vbCrLf & vbCrLf & "tested on NETz 0.4.8" & vbCrLf & vbCrLf & "sinister.ly <3" & vbCrLf & vbCrLf & "---------------" & vbCrLf & vbCrLf & "extracted!"
+        Timer1.Start()
+        Return 1
+    End Function
+    Private Sub Form1_DragEnter(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles Me.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+
+    Private Function MangleDllName(ByVal dll As String) As String
+        Dim text As String = dll.Replace(" ", "!1")
+        text = text.Replace(",", "!2")
+        text = text.Replace(".Resources", "!3")
+        text = text.Replace(".resources", "!3")
+        Return text.Replace("Culture", "!4")
+    End Function
+
+    Private Function GetResource(ByVal id As String) As Byte()
+        Dim array As Byte() = Nothing
+        If rm Is Nothing Then
+            rm = New ResourceManager("app", Assembly.LoadFile(targettedAssembly))
+        End If
+        Try
+            inResourceResolveFlag = True
+            Dim name As String = MangleDllName(id)
+            If array Is Nothing AndAlso xrRm IsNot Nothing Then
+                For i As Integer = 0 To xrRm.Count - 1
+                    Try
+                        Dim resourceManager As ResourceManager = CType(xrRm(i), ResourceManager)
+                        If resourceManager IsNot Nothing Then
+                            array = CType(resourceManager.GetObject(name), Byte())
+                        End If
+                    Catch
+                    End Try
+                    If array IsNot Nothing Then
+                        Exit For
+                    End If
+                Next
+            End If
+            If array Is Nothing Then
+                array = CType(rm.GetObject(name), Byte())
+            End If
+        Finally
+            inResourceResolveFlag = False
+        End Try
+        Return array
+    End Function
+
+    Private Function GetAssembly(ByVal data As Byte()) As Assembly
+        Dim memoryStream As MemoryStream = Nothing
+        Dim result As Assembly = Nothing
+        Try
+            memoryStream = UnZip(data)
+            memoryStream.Seek(0L, SeekOrigin.Begin)
+            If IO.Directory.Exists("NETz_Unpacker") = False Then
+                IO.Directory.CreateDirectory("NETz_Unpacker")
+            End If
+            IO.File.WriteAllBytes("NETz_Unpacker/" & IO.Path.GetFileName(targettedAssembly), memoryStream.ToArray())
+            result = Assembly.Load(memoryStream.ToArray())
+        Finally
+            If memoryStream IsNot Nothing Then
+                memoryStream.Close()
+            End If
+            memoryStream = Nothing
+        End Try
+        Return result
+    End Function
+
+    Private Function UnZip(ByVal data As Byte()) As MemoryStream
+        If data Is Nothing Then
+            Return Nothing
+        End If
+        Dim memoryStream As MemoryStream = Nothing
+        Dim memoryStream2 As MemoryStream = Nothing
+        Dim inflaterInputStream As InflaterInputStream = Nothing
+        Try
+            memoryStream = New MemoryStream(data)
+            memoryStream2 = New MemoryStream()
+            inflaterInputStream = New InflaterInputStream(memoryStream)
+            Dim array As Byte() = New Byte(data.Length - 1) {}
+            While True
+                Dim num As Integer = inflaterInputStream.Read(array, 0, array.Length)
+                If num <= 0 Then
+                    Exit While
+                End If
+                memoryStream2.Write(array, 0, num)
+            End While
+            memoryStream2.Flush()
+            memoryStream2.Seek(0L, SeekOrigin.Begin)
+        Finally
+            If memoryStream IsNot Nothing Then
+                memoryStream.Close()
+            End If
+            If inflaterInputStream IsNot Nothing Then
+                inflaterInputStream.Close()
+            End If
+            memoryStream = Nothing
+            inflaterInputStream = Nothing
+        End Try
+        Return memoryStream2
+    End Function
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Label1.Text = "unpacker by misonothx" & vbCrLf & vbCrLf & "tested on NETz 0.4.8" & vbCrLf & vbCrLf & "sinister.ly <3" & vbCrLf & vbCrLf & "---------------" & vbCrLf & vbCrLf & "drop file"
+        Timer1.Stop()
+    End Sub
+End Class
+
